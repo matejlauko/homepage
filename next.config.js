4e2e39de9b774ac5feeaa1776589c5dfe3e014d9
Matt Lauko <dev@lauko.io>
2023-06-04 23:45:55 +0200

    fix URL

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_URL: process.env.URL?.startsWith('http')
-      ? process.env.URL
-      : `https://${process.env.VERCEL_URL || ''}`,
+    NEXT_PUBLIC_URL: `${process.env.NODE_ENV === 'development' ? 'http' : 'https'}://${
+      process.env.HOST
+    }`,
   },
 }
 
