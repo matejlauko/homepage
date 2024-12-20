57a9dd1931405da728fd69672207921e4e5ddd5d
Matej Lauko <dev@lauko.io>
2024-12-20 21:20:43 +0100

    linear

---
+++ src/app/linear-cv/page.tsx
@@ -0,0 +1,168 @@
+export default function LinearCV() {
+  return (
+    <div className="container">
+      <article className="prose mx-auto py-8 prose-sm prose-h3:text-base">
+        <h1 className="text-2xl">Why I want to join Linear</h1>
+
+        <p>
+          A big fan of Linear here. As a very product/UX minded engineer, I
+          couldn&apos;t stand most software I &quot;had to use&quot; for work.
+          It seemed like everything must be some combination of slow, bloated
+          and hard to use or too simple and not supporting proper workflows.
+        </p>
+
+        <p>
+          Linear felt like the first software that &quot;gets it&quot;. It set
+          the standard for work software that doesn&apos;t make compromises.
+          <br />
+          Over the years I&apos;ve used Linear on multiple projects, big and
+          small. Always loved it. Never felt frustrated or needing more.
+        </p>
+
+        <p>
+          And I kept thinking, how great would be to work on a product like
+          this.
+          <br />
+          At the same time, the bar for the quality of the people working there
+          seemed too high and I felt like an imposter if I would&apos;ve applied
+          for a job.
+        </p>
+
+        <p>
+          This year, after being able to make an impact in Miro after building
+          the foundations of video calls on the board, I decided to shoot for
+          the stars and apply to Linear next year. And as luck would have it, I
+          got an email from Amelia and here we are.
+        </p>
+
+        <h4>Side note about product work</h4>
+
+        <p>
+          I didn&apos;t know how much product work is part of the
+          engineer&apos;s role in Linear.
+          <br />
+          But I love it!
+        </p>
+
+        <p>
+          It&apos;s always been my goal to not just build good software, but
+          build great products with nice UI and UX, that users love.
+          <br />
+          At Productboard I was learning from great PMs and helped shape parts
+          of the product. I worked on product and design iterations at Around.
+          Learned about product analytics or doing customer interviews.
+          <br />
+          Even though about switching to a PM role but would miss the actual
+          building.
+        </p>
+        <p>
+          What I&apos;m trying to say is that even though I don&apos;t have too
+          much direct product management experience, I&apos;ve picked up the
+          basics and I&apos;m eager to learn and grow.
+        </p>
+
+        <h2>Experience</h2>
+
+        <h3>Founding engineer / CTO in StartupJobs/Hiri</h3>
+        <em className="block -mt-2">
+          (Czech job board focused on tech companies)
+        </em>
+
+        <ul>
+          <li>
+            Built or led development of multiple web apps. First PHP & jQuery,
+            then React and Node.
+          </li>
+          <li>Hired and managed developers, managed infra.</li>
+        </ul>
+
+        <h3>Frontend developer in Productboard</h3>
+        <ul>
+          <li>Leading implementation of new rich-text editor</li>
+          <li>Converted whole codebase to TS with the team</li>
+          <li>
+            Led delivery of many features, mainly in the &quot;Insights&quot;
+            area
+          </li>
+        </ul>
+
+        <h3>Engineering manager in Productboard</h3>
+        <ul>
+          <li>
+            Leading team of 5-7 engineers, hiring, people and tech management
+          </li>
+          <li>
+            Part of the whole product process from discovery, helping PM and
+            designer
+          </li>
+        </ul>
+
+        <h3>Product engineer in Around</h3>
+        <ul>
+          <li>
+            Built many features in React/Electron app + Node servers/Postgres DB
+          </li>
+          <li>
+            Managing the whole projects alone, shaping the ideas with PM/design
+            through prototypes until final solution
+          </li>
+        </ul>
+
+        <h3>Frontend (product) engineer in Miro</h3>
+        <ul>
+          <li>
+            Leading implementation of video calls into Miro board, including
+            architecture, permissions and release management
+          </li>
+          <li>
+            Took it from the first POC, couple iterations until public beta
+            currently
+          </li>
+          <li>
+            Collaborating closely on features like the call setup dialog, call
+            scheduling or discussion view
+          </li>
+        </ul>
+
+        <h2>Personal projects</h2>
+
+        <h3>duotone</h3>
+        <div className="flex">
+          <a href="https://github.com/matejlauko/duotone" target="_blank">
+            GitHub
+          </a>
+
+          <div className="w-[2px] bg-gray-200 mx-2" />
+
+          <a href="https://duotone.lauko.io/examples/duotone" target="_blank">
+            Demo
+          </a>
+        </div>
+        <p>UI theme editor</p>
+
+        <h3>Moods for Spotify</h3>
+        <div className="flex">
+          <a
+            href="https://github.com/matejlauko/moods-for-spotify"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </div>
+        <p>Electron app to setup and switch Spotify playlists quickly</p>
+
+        <h3>
+          Base <span className="text-gray-500 text-sm">(WIP)</span>
+        </h3>
+
+        <div className="flex">
+          <a href="https://baseapp-eta.vercel.app/" target="_blank">
+            Demo
+          </a>
+        </div>
+
+        <p>Notes/todo list with rich-text content and hierarchy</p>
+      </article>
+    </div>
+  );
+}
